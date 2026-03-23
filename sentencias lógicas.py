58e0e76c4ef5659d0069d2a20ae05a5815a800f8
FATIMATOBIAS <fatimatobias79@gmail.com>
2026-03-23 12:46:51 -0600

    ejercicios de condicionales 1.py

---
+++ sentencias lógicas.py
@@ -0,0 +1,39 @@
+
+
+edad = 16
+licencia = 1
+
+if edad >= 16:
+    print("Tiene licencia")
+
+nombre = "sofia"
+apellido = "Martinez"
+
+if nombre == "sofia" and apellido == "Martinez":
+    print("sofia martinez tiene licencia")
+
+menu1 = "sopa"
+menu2 = "ensalada"
+
+if menu1 == "sopa" and menu2 == "ensalada":
+    print("el menu de hoy es sopa y ensalada ")
+
+animal1 = "tortuga"
+animal2 = "perro"
+if animal1 == "tortuga" and animal2 == "perro":
+    print("Mis mascotas son una tortuga y un perro")
+
+pastel1 = "queso"
+pastel2 = "zanahoria"
+if pastel1 == "queso" and pastel2 == "zanahoria":
+    print("Los pasteles son de queso y zanahoria")
+
+amiga1 = "valery"
+amigo2 = "jesus"
+if amiga1 == "valery" and amigo2 == "jesus":
+    print("Mis amigos son valery y jesus")
+
+fruta1 = "sandia"
+fruta2 = "naranja"
+if fruta1 == "sandia" and fruta2 == "naranja":
+    print("Me gusta la sandia y la naranja")
