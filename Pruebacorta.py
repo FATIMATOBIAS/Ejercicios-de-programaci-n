e9de1787cacd63d17e94057b2a27e3572615a093
FATIMATOBIAS <fatimatobias79@gmail.com>
2026-03-26 11:24:50 -0600

    prueba corta programación

---
+++ Pruebacorta.py
@@ -0,0 +1,36 @@
+
+print("Ingrese un número mayor a 0")
+numero = int(input("Ingrese el número:"))
+if numero >= 0:
+    print("El número es mayor a 0")
+
+
+int(input("Ingrese su edad:"))
+if numero <= 18:
+    print("Es mayor de edad")
+if numero >= 18:
+    print("Es menor de edad")
+
+nota = int(input("Ingrese su nota:"))
+if nota >= 90:
+    print("Excelente")
+elif nota >= 70:
+    print("Bueno")
+elif nota >= 69:
+    print("Regular")
+elif nota <= 60:
+    print("Reprobado")
+
+
+
+numero = int(input("ingrese un número:"))
+if numero >= 0:
+    print("Es positivo")
+elif numero <= 0:
+    print("Es negativo")
+if numero % 2 == 0:
+    print("Es par")
+else:
+    print("Es impar")
+
+
